bf2ed9c324c80d1a7a5b89a955d9349c4eb62446
Dan <36074341+DanPeia96@users.noreply.github.com>
2021-04-16 02:09:15 +0200

    Add files via upload

---
+++ src/main/java/es/uji/ei1027/ControlAcceso/controller/UsuarioController.java
@@ -0,0 +1,76 @@
+package es.uji.ei1027.ControlAcceso.controller;
+
+import javax.servlet.http.HttpSession;
+
+import es.uji.ei1027.ControlAcceso.dao.UsuarioDao;
+import es.uji.ei1027.ControlAcceso.model.Usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("user")
+public class UsuarioController {
+    private UsuarioDao userDao;
+
+    @Autowired
+    public void setUsuario(UsuarioDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @RequestMapping("/list")
+    public String listUusarios(HttpSession session, Model model) {
+        model.addAttribute("users", userDao.getUsuarios());
+        return "usuario/list";
+    }
+
+    @RequestMapping(value="/add")
+    public String addUsuario(Model model) {
+        model.addAttribute("usuario", new Usuario());
+        return "usuario/add";
+    }
+
+    @RequestMapping(value="/add", method= RequestMethod.POST)
+    public String processAddSubmit(@ModelAttribute("usuario") Usuario user,
+                                   BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "usuario/add";
+        UsuarioDao.addUsuario(user);
+        return "redirect:list";
+    }
+
+    @RequestMapping(value="/update/{dni}", method = RequestMethod.GET)
+    public String editUsuario(Model model, @PathVariable String dni) {
+        model.addAttribute("usuario", userDao.getUsuarioDni(dni));
+        return "usuario/update";
+    }
+
+    @RequestMapping(value="/busca/{dni}", method = RequestMethod.GET)
+    public String buscaUsuario(Model model, @PathVariable String dni) {
+        model.addAttribute("usuario", userDao.getUsuarioDni(dni));
+        return "usuario/busca";
+    }
+
+    @RequestMapping(value="/update", method = RequestMethod.POST)
+    public String processUpdateSubmit(
+            @ModelAttribute("usuario") Usuario usuario,
+            BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "usuario/update";
+        userDao.updateUsuario(usuario);
+        return "redirect:list";
+    }
+
+    @RequestMapping(value="/delete/{dni}")
+    public String processDelete(@PathVariable String dni) {
+        userDao.deleteUsuario(dni);
+        return "redirect:../list";
+    }
+}
+
